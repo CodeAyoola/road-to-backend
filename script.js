eef1d6aab71be247fa229ad6e0fbefbc962d5c58
CodeAyoola <rajiayoola7@gmail.com>
2025-12-27 07:38:24 +0100

    learn: values and variable delcaration

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 let js = "amazing";
-if (js === 'amazing') alert('Javascript is FUN');
 
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1413;
+
+// make sure your variable names are descriptive 
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+// better than 
+
+let job1 = 'Programmer'
+let job2 = 'Teacher'
