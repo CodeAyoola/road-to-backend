7d7b1c8f43c4e105b4e36f223c4f0ce33a3061be
CodeAyoola <rajiayoola7@gmail.com>
2025-12-31 10:39:49 +0100

    practice: third coding challenge

--- script.js
+++ script.js
@@ -292,30 +292,93 @@
 // }
 
 
-// Boolean Logic  
+// Boolean Logic
 // Logical Operator
 
-const hasDriverLicense = true; // A
-const hasGoodVison = true; // B
+// const hasDriverLicense = true; // A
+// const hasGoodVison = true; // B
 
-console.log(hasDriverLicense && hasGoodVison);
-console.log(hasDriverLicense || hasGoodVison);
-console.log(!hasDriverLicense && hasGoodVison);
+// console.log(hasDriverLicense && hasGoodVison);
+// console.log(hasDriverLicense || hasGoodVison);
+// console.log(!hasDriverLicense && hasGoodVison);
 
 
 
-// if (hasDriverLicense && hasGoodVison) {
+// // if (hasDriverLicense && hasGoodVison) {
+// //     console.log(`Sarah should drive!`)
+// // } else {
+// //     console.log(`Someone else should drive...`)
+// // }
+
+// const isTired = false // C
+
+// console.log(hasDriverLicense || hasGoodVison || isTired);
+
+// if (hasDriverLicense && hasGoodVison && !isTired) {
 //     console.log(`Sarah should drive!`)
 // } else {
 //     console.log(`Someone else should drive...`)
 // }
 
-const isTired = false // C
+// Third Coding Challenge
 
-console.log(hasDriverLicense || hasGoodVison || isTired);
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
 
-if (hasDriverLicense && hasGoodVison && !isTired) {
-    console.log(`Sarah should drive!`)
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, 
+// and print it to the console. Don't forget that there can be a draw, so test for that 
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+// team only wins if it has a higher score than the other team, and the same time a 
+// score of at least 100 points. Hint: Use a logical operator to test for minimum 
+// score, as well as multiple else-if blocks 😉
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+// both teams have the same score and both have a score greater or equal 100 
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+let averageDolphins = Math.trunc((96 + 108 + 89) / 3);
+let averageKoalas = Math.trunc((88 + 91 + 110) / 3);
+
+console.log(averageDolphins, averageKoalas)
+
+if (averageDolphins > averageKoalas) {
+    console.log(`Dolphins wins 🏆`)
+} else if (averageKoalas > averageDolphins) {
+    console.log(`Koalas wins 🏆`)
 } else {
-    console.log(`Someone else should drive...`)
+    `Both team tied`
+}
+
+averageDolphins = Math.trunc((97 + 112 + 101) / 3);
+averageKoalas = Math.trunc((109 + 95 + 123) / 3);
+
+console.log(averageDolphins, averageKoalas)
+
+if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+    console.log(`Dolphins wins 🏆`)
+} else if (averageKoalas > averageDolphins && averageKoalas >= 100) {
+    console.log(`Koalas wins 🏆`)
+} else {
+    `No team wins the trophy`
+}
+
+
+averageDolphins = Math.trunc((80 + 112 + 101) / 3);
+averageKoalas = Math.trunc((90 + 95 + 106) / 3);
+
+console.log(averageDolphins, averageKoalas)
+
+if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+    console.log(`Dolphins wins 🏆`)
+} else if (averageKoalas > averageDolphins && averageKoalas >= 100) {
+    console.log(`Koalas wins 🏆`)
+} else if (averageDolphins === averageKoalas && averageDolphins >= 100 && averageKoalas >= 100) {
+    console.log(`Both teams tied`)
+} else {
+    console.log(`No one wins the trophy`)
 }
