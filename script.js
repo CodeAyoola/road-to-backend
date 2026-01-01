411090b0f9fa78a78a029555935a42b49261213b
CodeAyoola <rajiayoola7@gmail.com>
2026-01-01 20:04:23 +0100

    practice: fourth coding challenge

--- script.js
+++ script.js
@@ -432,20 +432,29 @@
 
 // The Conditional (Ternary) Operator
 
-const age = 13;
-// age >= 18 ? console.log('I like to drink wine 🍷') :
-//     console.log('I like to drink water 💧')
+// const age = 13;
+// // age >= 18 ? console.log('I like to drink wine 🍷') :
+// //     console.log('I like to drink water 💧')
 
-const drink = age >= 18 ? "wine 🍷" : "water 💧";
-console.log(drink);
+// const drink = age >= 18 ? "wine 🍷" : "water 💧";
+// console.log(drink);
 
-let drink2;
-if (age >= 18) {
-    drink2 = 'wine 🍷';
-} else {
-    drink2 = 'water 💧';
-}
+// let drink2;
+// if (age >= 18) {
+//     drink2 = 'wine 🍷';
+// } else {
+//     drink2 = 'water 💧';
+// }
 
-console.log(drink2)
+// console.log(drink2)
 
-console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`)
+// console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`)
+
+
+// fourth coding challenge
+let bill = 430;
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
+
